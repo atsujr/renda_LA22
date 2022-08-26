31a20a1e53c1ecca9c255643e65278179897c511
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-08-27 02:19:35 +0900

    1回目

--- renda/ViewController.swift
+++ renda/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var num: Int! = 0
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 100
+        
     }
-
+    @IBAction func plus(_ sender: Any) {
+        num += 1
+        label.text = String(num)
+        
+    }
+    
 
 }
 
